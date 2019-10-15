13d4150b3cb176882b9104ffccdf95b971f2b67f
AndrewBeznosko <andrew.beznosko@gmail.com>
2019-10-15 14:05:10 +0300

    style fix

--- index.php
+++ index.php
@@ -16,23 +16,72 @@
     <link rel="stylesheet" href="css/first.css">
 </head>
 
+<style>
+    /* ======================================================== */
+header {
+    padding-top: 40px;
+}
+
+.header__content {
+    padding-top: 70px;
+}
+
+.header__content img {
+    margin-top: -60px;
+}
+
+#main_form {
+    max-width: 100%;
+    padding: 25px 25px 35px;
+}
+
+#main_form input {
+    padding: 20px 25px;
+}
+
+.bonus__subtitle {
+    margin: 0;
+}
+
+.header__content__bonus-box img {
+    top: 50%;
+    transform: translateY(-50%);
+    margin: 0;
+}
+
+#main_form .my-btn span {
+    font-size: 65px;
+    opacity: 0.1;
+}
+
+#main_form .my-btn {
+    justify-content: center;
+    font-size: 22px;
+    color: rgba(0, 0, 0, 0.6);
+    font-weight: 700;
+    font-family: "Futura PT";
+}
+
+/* ======================================================== */
+</style>
+
 <body class="relative" data-spy="scroll" data-target=".navbar-landing">
 <header>
     <div class="container">
         <div class="header__progress-box">
             <div class="header__progress-box__percent">
-                <p class="bg-percent">0%</p>
-                <p class="main-percent">прогресс регистрации <span>0%</span></p>
+                <p class="bg-percent">50%</p>
+                <p class="main-percent">прогресс регистрации <span>50%</span></p>
 
                 <div class="progress">
                     <div class="progress-bar progress-bar-striped progress-bar-animated"
                          role="progressbar"
-                         aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                         aria-valuenow="50%" aria-valuemin="50%" aria-valuemax="100"></div>
                 </div>
             </div>
         </div>
         <div class="header__content row d-flex align-items-start justify-content-between m-0">
-            <div class="col-12 col-lg-6 header__content__title mt-4">
+            <div class="col-12 col-lg-6 header__content__title">
                 <h2>ЗАВЕРШИТЕ РЕГИСТРАЦИЮ <br><span>НА ОНЛАЙН МАСТЕР-КЛАСС</span></h2>
                 <h1>"Как запустить бизнес на посуточной аренде и зарабатывать от $5'000 в месяц"</h1>
                 <img alt="" src="./img/person.png">
@@ -79,13 +128,7 @@
 
         $('#main_form').submit(function (event) {
             event.preventDefault();
-            let current_progress = 0;
-            let interval = setInterval(function () {
-                current_progress += 1;
-                $(".bg-percent, .main-percent span").html(current_progress + "%");
-                $(".progress-bar").width(current_progress + "%")
-                if (current_progress >= 100) {
-                    $.ajax({
+            $.ajax({
                         url: "./thankspage.php",
                         type: "POST",
                         data: {
@@ -100,9 +143,6 @@
                             window.location.href = "./thankspage.php";
                         }
                     });
-                    clearInterval(interval);
-                }
-            }, 44);
 
         });
         $('#customRadio1').change(function () {
